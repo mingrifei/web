c7138e9a866bce09c748381fbaa282c9441a5b59
mingrifei <mingrifei@sohu.com>
2018-05-08 22:51:14 +0800

    对证券公司搜索功能进行新增

--- test.py
+++ test.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
 import os,requests
 
-f = open("d:\\apt\\urls_pf.txt",'r')  # 返回一个文件对象
+f = open("C:\\Users\\Administrator\\Documents\\urls10.txt",'r')  # 返回一个文件对象
 lines = f.readlines()  # 调用文件的 readline()方法
 i=0
 vdata=''
 for line in lines:
     i=i+1
     #r=requests.get(line)
-    #print(r.text)
+    vdata = vdata + line
+    data = vdata
 
-    if(i%1==0):
-        vdata=vdata+line
-        data = line
+    if(i%2000==0):
+
+        #print(data)
         headers = {'content-type': 'text/plain','User-Agent': 'curl/7.12.1'}
         r = requests.post('http://data.zz.baidu.com/urls?site=www.futouzs.com&token=bC7EMK6gg5kB2HrJ', data=data, headers=headers)
         print(r.text)
+        print(i)
+        vdata = ''
     #print (line)
 
 f.close()
