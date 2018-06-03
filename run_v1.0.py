5664ad97f08247a63b1794eaf53a3069e4f6577d
mingrifei <mingrifei@sohu.com>
2018-06-04 00:50:58 +0800

    对证券公司搜索功能进行新增

--- run_v1.0.py
+++ run_v1.0.py
@@ -37,7 +37,7 @@ import lxml
 from tornado.options import define, options
 
 define("port", default=8080, help="run on the given port", type=int)
-define("mysql_host", default="122.112.238.137:4407", help="blog database host")
+define("mysql_host", default="127.0.0.1:4407", help="blog database host")
 define("mysql_database", default="bigdata", help="blog database name")
 define("mysql_user", default="root", help="blog database user")
 define("mysql_password", default="kingdom88", help="blog database password")
